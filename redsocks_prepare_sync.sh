80813da3d70abb111e5d07689c6d46d904256e3c
aidatorajiro <kawarusosu@zoho.com>
2022-11-07 12:59:06 +1100

    Prepare Tor's DNS Server and its virtual address hack feature

--- redsocks_prepare_sync.sh
+++ redsocks_prepare_sync.sh
@@ -10,7 +10,8 @@ redsocks -c /etc/redsocks.conf
 
 iptables -t nat -N REDSOCKS
 iptables -t nat -A REDSOCKS -d 0.0.0.0/8 -j RETURN
-iptables -t nat -A REDSOCKS -d 10.0.0.0/8 -j RETURN
+# iptables -t nat -A REDSOCKS -d 10.0.0.0/8 -j RETURN
+iptables -t nat -A REDSOCKS -d 10.0.0.0/10 -j RETURN
 iptables -t nat -A REDSOCKS -d 127.0.0.0/8 -j RETURN
 iptables -t nat -A REDSOCKS -d 169.254.0.0/16 -j RETURN
 iptables -t nat -A REDSOCKS -d 172.16.0.0/12 -j RETURN
