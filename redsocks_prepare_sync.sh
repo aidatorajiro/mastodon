db20afb82e39fcaa52628e13527168b8d8a8b8d9
aidatorajiro <kawarusosu@zoho.com>
2022-11-07 13:21:14 +1100

    add an useful comment

--- redsocks_prepare_sync.sh
+++ redsocks_prepare_sync.sh
@@ -11,7 +11,7 @@ redsocks -c /etc/redsocks.conf
 iptables -t nat -N REDSOCKS
 iptables -t nat -A REDSOCKS -d 0.0.0.0/8 -j RETURN
 # iptables -t nat -A REDSOCKS -d 10.0.0.0/8 -j RETURN
-iptables -t nat -A REDSOCKS -d 10.0.0.0/10 -j RETURN
+iptables -t nat -A REDSOCKS -d 10.0.0.0/10 -j RETURN # Redirect access to Tor's Virtual Network through SOCKS (so RETURN only some first part of 10.0.0.0/8. the rest (10.192.0.0/10) goes through redsocks)
 iptables -t nat -A REDSOCKS -d 127.0.0.0/8 -j RETURN
 iptables -t nat -A REDSOCKS -d 169.254.0.0/16 -j RETURN
 iptables -t nat -A REDSOCKS -d 172.16.0.0/12 -j RETURN
