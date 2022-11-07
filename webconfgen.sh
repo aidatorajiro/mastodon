2d1d678b9a139ec5ea2bf01dd3b1c0639dcfd337
aidatorajiro <kawarusosu@zoho.com>
2022-11-07 17:51:57 +1100

    use nginx to support stream

---
+++ webconfgen.sh
@@ -0,0 +1,14 @@
+ONIONAME=`cat tor_docker/hidden_service/hostname`
+
+cd ssl_certificate
+
+cat ssl.conf.temp | sed "s/<<<domain name>>>/$ONIONAME/" > ssl.conf
+
+openssl req -config ssl.conf -new -x509 -sha512 -newkey rsa:4096 -nodes \
+    -keyout privatekey.pem -days 365 -out certificate.pem
+
+rm ssl.conf
+
+cd ../nginx
+
+cat onionginx.conf.temp | sed "s/<<<domain name>>>/$ONIONAME/" > onionginx.conf
